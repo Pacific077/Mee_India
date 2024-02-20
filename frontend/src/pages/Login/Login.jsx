e3dd3090e6adede71262b466ec32ce5768903040
Pacific077 <2021ugcs077@nitjsr.ac.in>
2024-02-20 12:30:43 +0530

    made Signup page and handled switching between login and signup

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,27 +4,45 @@ import { FaEyeSlash } from "react-icons/fa6";
 import "./Login.css"
 const Login = () => {
   const [showPass,setshowPass] =useState(false);
+  const [IsSigningup,setIsSigningup] =useState(false);
   const handleIconClick = ()=>{
     setshowPass(!showPass)
+   
+  }
+  const SignupToLoginSwitch =()=>{
+    setIsSigningup(!IsSigningup);
   }
   return (
     <div className='LoginPage'>
       <div className="transparentbg"></div>
       <div className="loginform">
-        <h1 className='sec-head'>Login</h1>
+        <h1 className='sec-head'>{IsSigningup?"Signup":"Login"}</h1>
         <div className="loginfieldcont">
-          <label htmlFor="">Email</label>
+          {IsSigningup&&<div className="inpWithp">
+
+          <p htmlFor="">UserName</p>
+          <input type="text" placeholder='John Doe' />
+          </div>}
+          <div className="inpWithp">
+
+          <p htmlFor="">Email</p>
           <input className='emailInp' type="text" name="" id="" placeholder='abc@example.com' />
-          <label htmlFor="">Password</label>
+          </div>
+          
+<div className="inpWithp">
+
+          <p htmlFor="">Password</p>
+          
           <div className="passwrdcont">
           <input className='Passinp' type={showPass ? "text" : "password"} placeholder='Enter your Password' name=""  id="" />
           {showPass?<FaEyeSlash onClick={handleIconClick} className='eyeIcon'/>:<FaEye className='eyeIcon' onClick={handleIconClick}/>}
           </div>
+</div>
         </div>
-        <button className='btnPrim btn-sm mr-tp2'>Login</button>
+        <button className='btnPrim btn-sm mr-tp2'>{IsSigningup?"Sign Up":"Login"}</button>
         <div className='DontHavacnt'>
-          <p>Dont have an account ?</p>
-          <p href="" className='textBlue'>Sign Up</p>
+          <p>{IsSigningup?"Already have an account ?":"Dont have an account ?"}</p>
+          <p href="" className='textBlue' onClick={SignupToLoginSwitch}>{IsSigningup?"Login":"Sign Up"}</p>
         </div>
       </div>
     </div>
