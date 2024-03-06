556951699ba1d9227bd7ac24361bacd30c7148ac
Pacific077 <2021ugcs077@nitjsr.ac.in>
2024-03-07 01:27:44 +0530

    userdahsboard done

--- frontend/src/pages/BusinessDashboardSpecific/BusinessDashboardSpecific.jsx
+++ frontend/src/pages/BusinessDashboardSpecific/BusinessDashboardSpecific.jsx
@@ -1,10 +1,30 @@
-
+import { CiLocationOn } from "react-icons/ci";
 import t1 from "../../assets/t1.png";
 import "./BusinessDashboardSpecific.css";
 import BussinessDashIconsCard from "../../components/Card/BussinessDashIconsCard/BussinessDashIconsCard";
 import CompleteBusinessDetails from "../../components/CompleteBusinessDetails/CompleteBusinessDetails";
 import BusinessDashboardSpecificArr from "./BusinessDashbordSpecificArr";
+import { useEffect, useState } from "react";
+import { findByID } from "../../apis/BusinessApi";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 const BusinessDashboardSpecific = () => {
+  const [business,setBusiness] = useState(false)
+  const {BusinessId} =useParams()
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      try {
+        const resp = await findByID({bussinessId:BusinessId})
+        console.log("Resp",resp);
+        setBusiness(resp.data.businessDetail);
+        
+      } catch (error) {
+        toast.error("something Went Wrong")        
+      }
+
+    };
+    fetchData();
+  },[])
   return (
     <div className="BusinessDashboardSpecific">
       <div className="BusinessDashSpecBanner">
@@ -19,16 +39,11 @@ const BusinessDashboardSpecific = () => {
           <img src={t1} alt="" />
         </div>
       </div>
-      <div className="BusinessDashSpeScore">
-        <div className="ScoreAndDesc">
-          <div className="BussinessScorePercent">45%</div>
-          <div className="BussinessScoreDesc">
-            <p className="BussinessScoreDescHead">Busineess Score</p>
-            <p className="BussinessScoreDescInfo">Reach Out to more Customer</p>
-          </div>
-        </div>
-        <button className="BussinessScorebtn">Increase Score</button>
+      <div className="BusinessNameAndLocation">
+        <h1>{business?business.title:"none"}</h1>
+        <p><CiLocationOn/>{business?business.state:"none"}, {business?business.district:"none"}</p>
       </div>
+
       <div className="BusinessDashSpecDetails">
         <p className="BusinessDashSpecDetailsHead">Quick Link</p>
         <div className="BusinessDashSpecDetailsIconCont">
--- frontend/src/pages/BusinessDetailsEdit/Catalouge/Catalouge.jsx
+++ frontend/src/pages/BusinessDetailsEdit/Catalouge/Catalouge.jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Catalouge.css"
+import ReactLoading from "react-loading";
+import { toast } from "react-toastify";
+import { ImageToUrl } from '../../../utils/ImagetoUrl'
+import { useNavigate, useParams } from 'react-router-dom'
+import { BusienessEdit } from '../../../apis/BusinessApi';
 const Catalouge = () => {
+  const [ImageArr,setImageArr] = useState([])
+  const {BusinessId} =useParams();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const HandleFileChange =async (e)=>{
+    const files = Array.from(e.target.files);
+    setLoading(true)
+    const resp = await ImageToUrl(files);
+    const Item = resp[0];
+    setLoading(false)
+    setImageArr((prev) => [...prev, Item])
+  }
+  const handleSubmit=async ()=>{
+    try{
+      const Data = {
+        Catalouge:ImageArr,businessId:BusinessId
+      }
+      console.log("Data",Data);
+      const resp = await BusienessEdit(Data);
+      if(resp.status===200){
+        toast.success("Catalouge Added Successfully")
+        navigate(`/business-dashboard/${BusinessId}`)
+      }
+    }catch(er){
+      console.log(er.message)
+      toast.error("Something Went Wrong")
+    }
+  }
   return (
     <>
     <h1 className='BusinessEditHeading'>Add Catalouges </h1>
@@ -9,24 +42,34 @@ const Catalouge = () => {
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, fuga sectetur adipisicing elit. Adipisci, fuga.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, fuga etur adipisicing elit. Adipisci, fuga sectetur adipisicing elit. .</p>
     </div>
+    {loading && (
+          <div className="Loading" style={{ width: "100%" }}>
+            <ReactLoading
+              type="spokes"
+              color="purple"
+              height={120}
+              width={120}
+            />
+          </div>
+        )}
     <div className="BusinessEditCatalougeCont">
       <div className="AddCatalougeCard">
         <p className='plusSign'>+</p>
         <p>Add Photo</p>
-        <input type="file" className='BussinessEditCatalougeFile' />
+        <input type="file" className='BussinessEditCatalougeFile' onChange={HandleFileChange} />
       </div>
       <div className="AddCatalougeCard">
         <p className='plusSign'>+</p>
         <p>Add Photo</p>
-        <input type="file" className='BussinessEditCatalougeFile' />
+        <input type="file" className='BussinessEditCatalougeFile'onChange={HandleFileChange} />
       </div>
       <div className="AddCatalougeCard">
         <p className='plusSign'>+</p>
         <p>Add Photo</p>
-        <input type="file" className='BussinessEditCatalougeFile' />
+        <input type="file" className='BussinessEditCatalougeFile'onChange={HandleFileChange} />
       </div>
     </div>
-    <button className='btnPrim btn-lg'>Save</button>
+    <button className='btnPrim btn-lg' onClick={handleSubmit}>Save</button>
     </>
   )
 }
